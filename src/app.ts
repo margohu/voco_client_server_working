6c8655184f2026bc99fc519d3e4c31fdf1090d16
Künter Pärtel <kunter.partel@automi.eu>
2022-04-20 16:29:28 +0300

    Removed a few dependencies from package.json

--- src/app.ts
+++ src/app.ts
@@ -4,22 +4,10 @@ import {Application} from "express/ts4.0";
 import logger from "./util/logger";
 import socket from "./socket";
 import {WebSocketServer} from "ws";
-import cors from 'cors';
 
 const app: Application = express();
 
 const PORT: number = 1337 || parseInt(process.env.PORT as string, 10);
-
-// Add a list of allowed origins.
-// If you have more origins you would like to add, you can add them to the array below.
-const allowedOrigins = ['http://localhost:3000'];
-
-const options: cors.CorsOptions = {
-    origin: allowedOrigins
-};
-app.use(express.json());
-
-app.use(cors(options));
 
 
 const httpServer = createServer(app);
